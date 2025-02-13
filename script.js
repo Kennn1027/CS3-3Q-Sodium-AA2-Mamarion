c4876d9837a4a515adf7c1cdd32ce4ec287590b0
Kennn1027 <rkpmamarion22@src.pshs.edu.ph>
2025-02-13 11:24:18 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-        const Pi = 3.14;
-		var radius = 6.378e6;
+function Calculatethemonsters() {
+   
+    let P0 = parseFloat(prompt("Enter the initial population: "));
+    let r = parseFloat(prompt("Enter the growth rate in decimal (between 0 and 1): "));
+    let t = parseFloat(prompt("Enter the time in hours: "));
+
+    let P = Math.round(P0 * Math.pow(Math.E,r * t));
 	
-		let Circumference = 2*Pi*radius;
-		let SurfaceArea = 4*Pi*radius**2;
-		let Volume = (4/3)*Pi*radius**3;
-		
-		document.write("The radius of the sphere is " + radius + '<br>' );
-	    document.write("The circumference is " + Circumference + '<br>');
-	    document.write("The surface area is " + SurfaceArea + '<br>');
-	    document.write("The volume is " + Volume + '<br>');
-		
+    let region = prompt("Enter the monster's region/location: ");
+    let monstername = prompt("Enter the monster's name: ");
+
+    let monsterinformation = (region + " " + monstername).toUpperCase();
+
+    document.getElementById("result").innerHTML = 'After ' + t + ' hours, the population of ' + monsterinformation + ' has risen to ' + P;
+}
+ 
